d26e9283cfd53e19403d0f90343b56fc4cc48c8d
Denis Kostiv <deniskostiv@gmail.com>
2023-03-02 17:43:39 +0300

    add workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,10 +4,10 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-8gmelpy0)8icp0##o(0#xy8#19p7ma6v4+pbnc)gsc3k64l!ce')
+SECRET_KEY = os.getenv('SECRET_KEY', default='some_key')
 
 # DEBUG = True
 DEBUG = False
